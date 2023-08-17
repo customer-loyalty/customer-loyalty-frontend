4d12f4a31129572cd93f4564d302214dfc0e7eeb
Max <maximesin21@gmail.com>
2023-08-17 14:16:59 +0700

    Сведение лк клмпонентов в 1 мейн

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./Main.module.scss";
+import TableClients from "../TableClients/TableClients";
+import addIcon from "../../images/icon_plus.svg";
+
+function Main() {
+  return (
+    <main className={styles.main__container}>
+      <div className={styles.main__addContainer}>
+        <h1 className={styles.main__headline}>Клиенты</h1>
+        <button className={styles.main__button} type="submit">
+          <img src={addIcon} alt="Добавить клиента" /> Добавить клиента
+        </button>
+      </div>
+      <div className={styles.main__table}>
+        <p className={styles.main__clientCounter}>Всего клиентов: 3</p>
+        <TableClients />
+      </div>
+    </main>
+  );
+}
+
+export default Main;
