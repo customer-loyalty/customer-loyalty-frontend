24b44af16c28f56b60509a12913d33550f219ab0
khuseyn-khasiev <khuseyn-khasiev@mail.ru>
2023-08-19 12:21:35 +0300

    допилил меню для мобилки

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,15 +1,46 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import styles from './menu.module.scss';
 import userImage from '../../images/user__image.jpg';
 
-
-
 function Menu() {
-    const [burgerImageVisible, setBurgerImageVisible] = useState(false);
+    const [burgerImageVisible, setBurgerImageVisible] = useState(true);
+    const [isDisplayNone, setIsDisplayNone] = useState(false);
+    const [width, setWidth] = useState(window.innerWidth);
     const buttonLabel = burgerImageVisible ? "Открыть меню" : "Закрыть меню";
 
+    useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (width < 391) {
+            setBurgerImageVisible(false);
+            setIsDisplayNone(true);
+        } else {
+            setBurgerImageVisible(true);
+            setIsDisplayNone(false);
+        }
+    }, [width])
     const handleBurgerClick = () => {
-        setBurgerImageVisible(!burgerImageVisible);
+        if(burgerImageVisible){
+            setTimeout(() => {
+                setIsDisplayNone(true);
+            }, 1600)
+            setBurgerImageVisible(!burgerImageVisible);
+        }
+        if(!burgerImageVisible) {
+            setIsDisplayNone(false);
+            setTimeout(() => {
+                setBurgerImageVisible(!burgerImageVisible);
+            }, 0)
+        }
     };
     const handleBurgerKeyDown = (event) => {
         if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
@@ -18,19 +49,16 @@ function Menu() {
     }
 
     return (
-        <div className={`${styles.menu} ${burgerImageVisible && styles.menu_open}`}>
+        <div className={`${styles.menu} ${burgerImageVisible ? styles.menu_open : ''}`}>
             <div className={styles.menu__container}>
-                <div className={styles.logo}
-                     onClick={handleBurgerClick}
-                     onKeyDown={handleBurgerKeyDown}
-                     role="button"
-                     tabIndex={0}
-                     aria-label={buttonLabel}
-                >
+                <div className={styles.logo}>
                     <div className={styles.logo__icon}/>
                     <p className={styles.logo__text}>LOGO</p>
                 </div>
-                <div className={`${styles.user} ${!burgerImageVisible && styles.menu_opacity}`}>
+                <div className={
+                    `${styles.user} ${!burgerImageVisible ? styles.menu__user_opacity : ''}
+                    ${isDisplayNone ? styles.menu_none : ''}`
+                }>
                     <img className={styles.user__image} src={userImage} alt='картинка профиля'/>
                     <div className={styles.user__info}>
                         <p className={styles.user__name}>Любовь Светлова</p>
@@ -39,7 +67,8 @@ function Menu() {
                 </div>
                 <menu className={
                     `${styles.menu__nav} 
-                    ${!burgerImageVisible && styles.menu_opacityFast}`
+                    ${!burgerImageVisible ? styles.menu__nav_opacity : ''}
+                    ${isDisplayNone ? styles.menu_none : ''}`
                 }>
                     <ul className={styles.menu__list}>
                         <li className={`${styles.menu__item} ${styles.menu__item_active}`}>
@@ -60,20 +89,28 @@ function Menu() {
                         </li>
                     </ul>
                 </menu>
+                <div className={
+                    `${styles.telephoneInfo} 
+                ${!burgerImageVisible ? styles.menu__telephoneInfo_opacity : ''}
+                ${isDisplayNone ? styles.menu_none : ''}
+                `}>
+                    <p className={styles.telephoneText}>Нужна помощь? Звоните</p>
+                    <a className={styles.telephoneContact} href='tel:+79002541414'>8(900)254-14-14</a>
+                </div>
+                <div className={styles.menu__footer}
+                     onClick={handleBurgerClick}
+                     onKeyDown={handleBurgerKeyDown}
+                     role="button"
+                     tabIndex={0}
+                     aria-label={buttonLabel}
+                >
+                    <div className={styles.menu__footerContainer}>
+                        <div className={`${styles.menu__exitBurger} ${burgerImageVisible ? styles.menu__openBurger : ''}`}
+                        />
+                    </div>
+                </div>
             </div>
-            <div className={`${styles.telephoneInfo} ${!burgerImageVisible && styles.menu_opacity}`}>
-                <p className={styles.telephoneText}>Нужна помощь? Звоните</p>
-                <a className={styles.telephoneContact} href='tel:+79002541414'>8(900)254-14-14</a>
-            </div>
-            <div className={`${styles.menu__exitBurger} ${burgerImageVisible && styles.menu__openBurger}`}
-                 onClick={handleBurgerClick}
-                 onKeyDown={handleBurgerKeyDown}
-                 role="button"
-                 tabIndex={0}
-                 aria-label={buttonLabel}
-            />
         </div>
     )
 }
-
 export default Menu;
