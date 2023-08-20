7c4148e607240ba69fa3dbc626f6bae172576a70
Pazific <advolatum@yandex.ru>
2023-08-20 18:56:41 +0300

    refactor: comment promo page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-// import LKContainer from "./components/LKCointainer/LKContainer";
-import PromoPage from "./components/pages/promo/page";
+import LKContainer from "./components/LKCointainer/LKContainer";
+// import PromoPage from "./components/pages/promo/page";
 
 function App() {
-  // return <LKContainer />;
-  return <PromoPage />
+  return <LKContainer />;
+  // return <PromoPage />
 }
 
 export default App;
