6ca9ede0843c6f4301ba18c85f5e8cab0ee24be9
Alexandr Yakushkin <yalex007@mail.ru>
2023-09-13 17:33:56 +0300

    Добавление раздела аккаунт

---
+++ src/components/Account/Account.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import styles from "./Account.module.css";
+import Headline from "../Headline/Headline";
+import ButtonLK from "../ButtonLK/Button";
+import editHovered from "../../images/lk/editHovered.svg";
+import info from "../../images/lk/info.svg";
+import director from "../../images/lk/director.svg";
+import contacts from "../../images/lk/contacts.svg";
+import card from "../../images/lk/card.jpg";
+
+export default function Account() {
+  return (
+    <main className={styles.content}>
+      <Headline title="Аккаунт" />
+      <div className={styles.buttons}>
+        <ButtonLK
+          img={editHovered}
+          style={{ backgroundColor: "#5CA1EA", color: "#FFFFFF" }}
+          hoverStyle={{ backgroundColor: "#384098", color: "#FFFFFF" }}
+          text="Редактировать"
+        />
+      </div>
+      <div className={styles.info}>
+        <div className={styles.info__card}>
+          <div className={styles.info__header}>
+            <img className={styles.info__icon} src={info} alt="иконка" />
+            <h2 className={styles.info__title}>Информация о компании</h2>
+          </div>
+          <ul className={styles.info__content}>
+            <li className={styles.info__point}>
+              Название компании
+              <p className={styles.info__text}>ООО «Кофечай»</p>
+            </li>
+            <li className={styles.info__point}>
+              Основной вид деятельности
+              <p className={styles.info__text}>
+                Продажа кофе, чая и свежей выпечки
+              </p>
+            </li>
+            <li className={styles.info__point}>
+              Адрес
+              <p className={styles.info__text}>г. Москва, ул. Щукинская, 14</p>
+            </li>
+          </ul>
+        </div>
+
+        <div className={styles.info__card}>
+          <div className={styles.info__header}>
+            <img className={styles.info__icon} src={contacts} alt="иконка" />
+            <h2 className={styles.info__title}>Конаткты</h2>
+          </div>
+          <ul className={styles.info__content}>
+            <li className={styles.info__point}>
+              Почта
+              <p className={styles.info__text}>kofechay@mail.ru</p>
+            </li>
+            <li className={styles.info__point}>
+              Сайт
+              <p className={styles.info__text}>www.cofechay.ru</p>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.info__card}>
+          <div className={styles.info__header}>
+            <img className={styles.info__icon} src={director} alt="иконка" />
+            <h2 className={styles.info__title}>Директор</h2>
+          </div>
+          <ul className={styles.info__content}>
+            <li className={styles.info__point}>
+              ФИО
+              <p className={styles.info__text}>Иванов Владислав Геннадьевич</p>
+            </li>
+            <li className={styles.info__point}>
+              Должность
+              <p className={styles.info__text}>Генеральный директор</p>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.info__card}>
+          <div className={styles.info__header}>
+            <img className={styles.info__icon} src={card} alt="иконка" />
+            <div className={styles.info__headerText}>
+              <h2 className={styles.info__title}>Карты</h2>
+              <p className={styles.info__subtitle}>Всего 2 карты</p>
+            </div>
+          </div>
+          <ul className={styles.info__content}>
+            <li className={styles.info__point}>
+              Уровень 1<p className={styles.info__text}>Стандартная</p>
+            </li>
+            <li className={styles.info__point}>
+              Уровень 2<p className={styles.info__text}>Продвинутая</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
+}
